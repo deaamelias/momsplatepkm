918f6886c2ac9e981686d8031c163182d1d1177f
deaamelias <deamelyaa07@gmail.com>
2024-02-09 22:43:51 +0800

    index

--- proses_login.php
+++ proses_login.php
@@ -15,30 +15,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $hashed_password = hash('sha256', $password);
 
     // Query SQL untuk memeriksa kredensial pengguna
-    $query = "SELECT id, username FROM users WHERE username = '$username' AND password = '$hashed_password'";
+    $query = "SELECT id, username, role FROM users WHERE username = '$username' AND password = '$hashed_password'";
     $result = $conn->query($query);
 
     if ($result->num_rows > 0) {
         // Jika data ditemukan, ambil informasi pengguna
         $user = $result->fetch_assoc();
-    
-        // Ambil ID pengguna
-        $user_id = $user['id'];
-    
-        // Set session ID pengguna
-        $_SESSION['user_id'] = $user_id;
-    
-        // Redirect ke halaman dashboard
-        header("Location: dashboard.php");
-        exit();
+
+        // Set session untuk username
+        $_SESSION['username'] = $username;
+
+        // Redirect berdasarkan peran (role) pengguna
+        if ($user['role'] === 'admin') {
+            // Jika peran pengguna adalah admin, redirect ke admin.php
+            header("Location: admin.php");
+            exit();
+        } else {
+            // Jika peran pengguna adalah user, redirect ke dashboard.php
+            header("Location: dashboard.php");
+            exit();
+        }
     } else {
         // Jika data tidak ditemukan, set pesan kesalahan
         $_SESSION['login_error'] = "Username atau password salah.";
-    
+
         // Redirect ke halaman login kembali
         header("Location: index.php");
         exit();
     }
-    
 }
 ?>
